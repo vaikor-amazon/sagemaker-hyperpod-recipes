a9dab2ef94498a4d1a4eb736b1fba7f7ad36a084
Vaibhav Korrapati <vaikor@amazon.com>
2025-10-28 08:45:25 -0700

    Add supported region mappings for PDX and LHR

--- launcher/nova/constants/init_container_constants.py
+++ launcher/nova/constants/init_container_constants.py
@@ -1,2 +1,2 @@
-INIT_CONTAINER_REGION_ACCOUNT_MAP = {"us-east-1": "708977205387"}
+INIT_CONTAINER_REGION_ACCOUNT_MAP = {"us-east-1": "708977205387", "eu-west-2": "470633809225", "us-west-2": "176779409107"}
 INIT_CONTAINER_IMAGE_URI = "{account_id}.dkr.ecr.{region}.amazonaws.com/init-container-repo:latest"
